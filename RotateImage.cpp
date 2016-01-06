4e614906214a8129af3f3c5c2fb9fdae8f0f75c1
小麻酱 <jiangtengfei007@163.com>
2016-01-06 10:07:49 +0800

    add RotateImage code
    add RotateImage code

---
+++ RotateImage.cpp
@@ -0,0 +1,56 @@
+/*
+ 
+ You are given an n x n 2D matrix representing an image.
+ 
+ Rotate the image by 90 degrees (clockwise).
+ 
+ * 1 2 3     7 8 9     7 4 1
+ * 4 5 6  => 4 5 6  => 8 5 2
+ * 7 8 9     1 2 3     9 6 3
+ */
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        auto result = matrix;
+        
+        for (int i = 0; i < matrix.size(); i++) {
+            for (int j = 0; j < matrix.size(); j++) {
+                result[j][matrix.size() - i - 1] = matrix[i][j];
+            }
+        }
+        matrix = result;
+    }
+};
+
+
+/*
+ * clockwise rotate
+ * first reverse up to down, then swap the symmetry
+ * 1 2 3     7 8 9     7 4 1
+ * 4 5 6  => 4 5 6  => 8 5 2
+ * 7 8 9     1 2 3     9 6 3
+ */
+void rotate(vector<vector<int> > &matrix) {
+    reverse(matrix.begin(), matrix.end());
+    for (int i = 0; i < matrix.size(); ++i) {
+        for (int j = i + 1; j < matrix[i].size(); ++j)
+            swap(matrix[i][j], matrix[j][i]);
+    }
+}
+
+
+/*
+ * clockwise rotate
+ * first reverse up to down, then swap the symmetry
+ * 1 2 3     7 8 9     7 4 1
+ * 4 5 6  => 4 5 6  => 8 5 2
+ * 7 8 9     1 2 3     9 6 3
+ */
+void rotate(vector<vector<int> > &matrix) {
+    reverse(matrix.begin(), matrix.end());
+    for (int i = 0; i < matrix.size(); ++i) {
+        for (int j = i + 1; j < matrix[i].size(); ++j)
+            swap(matrix[i][j], matrix[j][i]);
+    }
+}
